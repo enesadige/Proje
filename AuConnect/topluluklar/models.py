c961f7e2c3e07c6f2d3e5aac49107ae508483b4b
enesadige <enesdasci.7jx@gmail.com>
2025-04-23 20:50:43 +0300

    son çalışan versiyon veritabanı frontend bağlantısı çalışıyor

--- AuConnect/topluluklar/models.py
+++ AuConnect/topluluklar/models.py
@@ -6,4 +6,11 @@ class Topluluklar(models.Model):
     name = models.CharField(max_length=100)
     img = models.CharField(max_length=100)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+    def get_image_path(self):
+        return "/img/" + str(self.img)
     
--- AuConnect/topluluklar/views.py
+++ AuConnect/topluluklar/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topluluklar
 
 def liste(request):
-    return render(request, "topluluklar/liste.html")
+
+    topluluklar = Topluluklar.objects.all()
+    context = {
+        "topluluklar" : topluluklar
+    }
+
+
+    return render(request, "topluluklar/liste.html", context)
