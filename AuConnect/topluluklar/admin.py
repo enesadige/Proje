ace2d8bdee6d08bcf2cd1d24a5ab0edcc943f5c9
enesadige <enesdasci.7jx@gmail.com>
2025-04-23 20:17:13 +0300

    model eklendi

--- AuConnect/topluluklar/admin.py
+++ AuConnect/topluluklar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topluluklar
 
-# Register your models here.
+class ToplulukAdmin(admin.ModelAdmin):
+
+    list_display = ("id","name")
+
+admin.site.register(Topluluklar,ToplulukAdmin)
--- AuConnect/topluluklar/models.py
+++ AuConnect/topluluklar/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Topluluklar(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    description = models.TextField()
+    
