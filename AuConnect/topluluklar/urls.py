f2f2bff8fc3acb500f9a4502e1887e7ca0e72df0
enesadige <enesdasci.7jx@gmail.com>
2025-04-28 21:07:11 +0300

    topluluk detay sayfası eklendi

--- AuConnect/topluluklar/urls.py
+++ AuConnect/topluluklar/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.liste, name = "topluluklar")
+    path("", views.liste, name = "topluluklar"),
+    path('<int:topluluklar_id>', views.detail, name='detail')
 ]
--- AuConnect/topluluklar/views.py
+++ AuConnect/topluluklar/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Topluluklar
+from django.http import Http404  
 
 def liste(request):
 
@@ -8,6 +9,16 @@ def liste(request):
     context = {
         "topluluklar" : topluluklar
     }
-
-
     return render(request, "topluluklar/liste.html", context)
+
+
+def detail(request, topluluklar_id):
+    try:
+        topluluklar = Topluluklar.objects.get(pk = topluluklar_id)
+    except Topluluklar.DoesNotExist:
+        raise Http404('Topluluk bulunamadı...')
+    
+    context = {
+        'topluluklar':topluluklar
+    }
+    return render(request, "topluluklar/detail.html", context)
